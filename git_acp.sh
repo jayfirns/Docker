4d1f524743722ab7077e12c8e4ed320e7c7d0d2d
jayfirns <126042840+jayfirns@users.noreply.github.com>
2023-10-04 19:55:29 -0600

    Update git_acp.sh
        We use the git symbolic-ref --short HEAD command to get the name of the current branch and store it in the current_branch variable.
        We check if the current_branch variable is empty, which would indicate that you are not currently on a branch. If you're not on a branch, it prevents the script from proceeding.
        We use the $current_branch variable when pushing to the remote repository, which ensures that the changes are pushed to the current branch rather than the main branch.

--- git_acp.sh
+++ git_acp.sh
@@ -2,6 +2,13 @@
 
 read -p "Enter your commit message: " commit_message
 
+current_branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+
+if [ -z "$current_branch" ]; then
+    echo "You are not on a branch. Please switch to a branch and try again."
+    exit 1
+fi
+
 git add .
 git commit -m "$commit_message"
 
@@ -10,11 +17,11 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-git push origin main
+git push origin "$current_branch"
 
 if [ $? -ne 0 ]; then
     echo "Push failed. Please resolve any issues and try again."
     exit 1
 fi
 
-echo "Changes successfully committed and pushed to the main branch."
+echo "Changes successfully committed and pushed to the current branch: $current_branch."
